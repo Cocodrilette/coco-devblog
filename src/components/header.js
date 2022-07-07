75bd413c2a131ebef8426c549eb5d7c81e58dd0d
Cocodrilette <cocodrilette@gmail.com>
2022-07-07 11:51:22 -0500

    version 1.0.2 - icon added - some styles fixed

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import { StaticImage } from 'gatsby-plugin-image'
 import { siteHeader } from '../comp-styles/main-style.module.css'
 import { useStaticQuery, graphql } from 'gatsby'
 
@@ -13,11 +14,15 @@ function Header() {
             }
           }
         `)
-        
+
     return (
         <header className={siteHeader}>
             <div>
                 <Link to="/" title='Go to home'>
+                    <StaticImage
+                        src='../images/logo.png'
+                        alt='A very cool cyberpunk dude with a very fancy glasses sitting on the floor using a very cool computer'
+                    />
                     {titleData.site.siteMetadata.title}
                 </Link>
             </div>
